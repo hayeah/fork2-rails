029ad1e6625308b057cf1df2f2871a8115e97b3b
Howard Yeh <howard@metacircus.com>
2014-03-19 21:14:43 +0800

    get rid of locale warning
    http://stackoverflow.com/questions/20361428/rails-i18n-validation-deprecation-warning

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,9 @@ module Fork
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.i18n.enforce_available_locales = true
+
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
 
     config.active_record.schema_format = :sql
