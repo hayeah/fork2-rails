762d010d0a47ed5f14f54da1356af551081fe392
Howard Yeh <howard@metacircus.com>
2014-03-24 21:46:14 +0800

    configure omniauth full_host for production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,3 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :github, CONFIG['github']['key'], CONFIG['github']['secret']
 end
+
+if Rails.env.production?
+  OmniAuth.config.full_host = "http://fork2.com"
+end
