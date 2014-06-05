fbaa506023b839b2cf552060531030207278fc9b
dingxijin <fatelovely1128@gmail.com>
2014-06-05 23:48:59 +0800

    add timeline

--- app/controllers/admin/status_controller.rb
+++ app/controllers/admin/status_controller.rb
@@ -3,6 +3,9 @@ class Admin::StatusController < ApplicationController
   def index
     @users=CohortUser.all
     @total=13
+    start_date=Date.new(2014,5,1)
+    @start_mjd=start_date.mjd
+    @intervals=Date.today.mjd-@start_mjd
   end
 
 end
