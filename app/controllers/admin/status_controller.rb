41df5e3108ea271a313b52bd369649e63655dbbd
dingxijin <fatelovely1128@gmail.com>
2014-06-07 10:15:54 +0800

    delete courses block,add its function to timeline

--- app/controllers/admin/status_controller.rb
+++ app/controllers/admin/status_controller.rb
@@ -2,7 +2,7 @@ class Admin::StatusController < ApplicationController
 
   def users
     @users=CohortUser.all
-    @total=13
+    @total_courses=13
     start_date=Date.new(2014,5,1)
     @start_mjd=start_date.mjd
     @intervals=Date.today.mjd-@start_mjd
